fff170a3f0d04a2059ee6a32077ea5f9daa592df
Eunice Vassoa <eunicevassoa@gmail.com>
2021-12-05 15:07:37 +0200

    removing unused code

--- test/easy_infinite_scroll_test.dart
+++ test/easy_infinite_scroll_test.dart
@@ -3,10 +3,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:easy_infinite_scroll/easy_infinite_scroll.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  test('adds one to input values', () {});
 }
