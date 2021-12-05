7747b93720ec376d29b444c23100d0f4a8b3c81d
Eunice Vassoa <eunicevassoa@gmail.com>
2021-12-05 15:07:28 +0200

    creating infinite scroll listview

--- lib/easy_infinite_scroll.dart
+++ lib/easy_infinite_scroll.dart
@@ -1,9 +1,11 @@
 library easy_infinite_scroll;
 
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class EasyInfiniteScroll extends StatefulWidget {
-  final Future<List> fetchData;
+  final Future<List<Widget>> fetchData;
   final bool hasMoreData;
 
   EasyInfiniteScroll({
@@ -18,33 +20,71 @@ class EasyInfiniteScroll extends StatefulWidget {
 class _EasyInfiniteScrollState extends State<EasyInfiniteScroll> {
   late ScrollController _scrollController;
   final List _items = [];
+  bool _isFirstLoad = true;
 
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration.zero, () async => await _fetchData() );
+    Future.delayed(Duration.zero, () async {
+      await _fetchData();
+      setState(() => _isFirstLoad = false );
+    });
     _scrollController = ScrollController();
     _scrollController.addListener(() async {
-      var triggerFetchMoreSize = 0.9 * _scrollController.position.maxScrollExtent;
-
-      if (_scrollController.position.pixels > triggerFetchMoreSize) {
-        _fetchData();
+      if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+        await _fetchData();
       }
     });
   }
 
+  Widget _buildLoader() {
+    Widget _loader;
+    if (Platform.isIOS) {
+      _loader = CupertinoActivityIndicator();
+    }
+    else {
+      _loader = CircularProgressIndicator();
+    }
+
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(vertical: 5),
+      child: _loader,
+    );
+  }
+
   Future<void> _fetchData() async {
+    if (_isFirstLoad) {
+      setState(() =>_items.add( _buildLoader() ));
+    }
+
     await widget.fetchData.then((value) {
-      setState(() => _items.addAll(value));
+      setState(() {
+        _items.removeLast();
+        _items.addAll(value);
+        if (widget.hasMoreData) {
+          print('Not done yet');
+          setState(() =>_items.add( _buildLoader() ));
+        }
+        else {
+          print('Done');
+          setState(() =>_items.add(
+            Center(
+              child: Text('Loaded last item')
+            )
+          ));
+        }
+      });
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       controller: _scrollController,
       itemCount: _items.length,
-      itemBuilder: (context, index) {
-        return _items[index];
+      itemBuilder: (_, i) {
+        return _items[i];
       }
     );
   }
